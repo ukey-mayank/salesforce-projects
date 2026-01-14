294da5230ad1e9b2cc7fdb6b8e3708fe0cc1ba7f
MayankUkey <mayankukey15@gmail.com>
2026-01-14 11:40:21 +0530

    rendering from user-input

--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,5 +1,21 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+import getAccounts from "@salesforce/apex/AccountController.getAccounts";
 
 export default class ParentComponent extends LightningElement {
-  accountId = "001C100000ObUBbIAN";
+  numberOfAccounts = 0;
+  accounts = [];
+  @wire(getAccounts, { numberOfAccounts: "$numberOfAccounts" })
+  wiredAccounts({ data, error }) {
+    if (data) {
+      this.accounts = data;
+      this.error = undefined;
+    } else if (error) {
+      this.error = error;
+      this.data = undefined;
+    }
+  }
+
+  handleAccountInputChange(event) {
+    this.numberOfAccounts = event.detail.value;
+  }
 }
