282bfd696406201da838c5bc9b0b4713adbf9054
MayankUkey <mayankukey15@gmail.com>
2026-01-15 17:25:26 +0530

    Rendering elements properly with the help of Loops

--- force-app/main/default/lwc/createContactWithForm/createContactWithForm.js
+++ force-app/main/default/lwc/createContactWithForm/createContactWithForm.js
@@ -1,3 +1,36 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
 
-export default class CreateContactWithForm extends LightningElement {}
+export default class CreateContactWithForm extends LightningElement {
+  inputData = [
+    {
+      Id: "1",
+      type: "text",
+      label: "Enter First Name",
+      classList: "slds-var-m-around_medium slds-var-p-horizontal_small"
+    },
+    {
+      Id: "2",
+      type: "text",
+      label: "Enter Last Name",
+      classList: "slds-var-m-around_medium slds-var-p-horizontal_small"
+    },
+    {
+      Id: "3",
+      type: "email",
+      label: "Enter Email",
+      classList: "slds-var-m-around_medium slds-var-p-horizontal_small"
+    },
+    {
+      Id: "4",
+      type: "tel",
+      label: "Enter Phone Number",
+      classList: "slds-var-m-around_medium slds-var-p-horizontal_small"
+    },
+    {
+      Id: "5",
+      type: "text",
+      label: "Enter More Details about Yourself",
+      classList: "slds-var-m-around_medium slds-var-p-horizontal_small"
+    }
+  ];
+}
