95f4c2e2b275d025b02cf36e9ad714a5e57c06f7
MayankUkey <mayankukey15@gmail.com>
2026-01-16 13:32:35 +0530

    Complete functionality to get weather of any location in the world with a Free API called WeatherStack

--- force-app/main/default/lwc/weatherService/weatherService.js
+++ force-app/main/default/lwc/weatherService/weatherService.js
@@ -9,6 +9,19 @@ const weatherOptions = [
 
 export default class WeatherService extends LightningElement {
   currentCity = "Nagpur";
+  country;
+  timezone_id;
+  observation_time;
+  temperature;
+  weather_description;
+  sunrise;
+  sunset;
+  pm2_5;
+  wind_speed;
+  pressure;
+  humidity;
+  visibility;
+  is_day;
   weatherOptions = weatherOptions;
   selectedType = "current";
 
@@ -26,7 +39,20 @@ export default class WeatherService extends LightningElement {
       dataType: this.selectedType
     })
       .then((res) => {
-        console.log("Result: ", res);
+        this.currentCity = res.name;
+        this.country = res.country;
+        this.timezone_id = res.timezone_id;
+        this.observation_time = res.observation_time;
+        this.temperature = res.temperature;
+        this.weather_description = res.weather_description;
+        this.sunrise = res.sunrise;
+        this.sunset = res.sunset;
+        this.pm2_5 = res.pm2_5;
+        this.wind_speed = res.wind_speed;
+        this.pressure = res.pressure;
+        this.humidity = res.humidity;
+        this.visibility = res.visibility;
+        this.is_day = res.is_day;
       })
       .catch((error) => {
         console.log(error);
