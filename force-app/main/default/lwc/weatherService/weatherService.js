665d3d815b6e18e19a479f06270a21dfa1443c46
MayankUkey <mayankukey15@gmail.com>
2026-01-16 17:29:41 +0530

    Optimised rendering with loops and data stored in Js

--- force-app/main/default/lwc/weatherService/weatherService.js
+++ force-app/main/default/lwc/weatherService/weatherService.js
@@ -15,6 +15,51 @@ export default class WeatherService extends LightningElement {
   weather;
   isLoading = false;
 
+  get weatherFields() {
+    if (!this.weather) return [];
+
+    return [
+      { key: "city", label: "City", value: this.weather.name },
+      { key: "country", label: "Country", value: this.weather.country },
+      { key: "timezone", label: "Timezone", value: this.weather.timezone_id },
+      {
+        key: "observation",
+        label: "Observation",
+        value: this.weather.observation_time
+      },
+      {
+        key: "temperature",
+        label: "Temperature",
+        value: this.weather.temperature
+      },
+      {
+        key: "weather_desc",
+        label: "Weather Description",
+        value: this.weather.weather_description
+      },
+      {
+        key: "sunrise",
+        label: "Sunrise",
+        value: this.weather.sunrise
+      },
+      { key: "sunset", label: "Sunset", value: this.weather.sunset },
+      { key: "pm2_5", label: "Air Quality", value: this.weather.pm2_5 },
+      {
+        key: "wind_speed",
+        label: "Wind Speed",
+        value: this.weather.wind_speed
+      },
+      { key: "pressure", label: "Pressure", value: this.weather.pressure },
+      { key: "humidity", label: "Humidity", value: this.weather.humidity },
+      {
+        key: "visibility",
+        label: "Visibility",
+        value: this.weather.visibility
+      },
+      { key: "is_day", label: "Is Day?", value: this.weather.is_day }
+    ];
+  }
+
   handleInputChange(event) {
     this.currentCity = event.target.value;
   }
