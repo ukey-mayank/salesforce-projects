7c0014f45e7c7e3fdde08cfc6f937309c64eb7b7
MayankUkey <mayankukey15@gmail.com>
2026-01-17 20:08:23 +0530

    Initial commit of Aviation Data having multiple records of data

--- force-app/main/default/lwc/aviationService/aviationService.js
+++ force-app/main/default/lwc/aviationService/aviationService.js
@@ -1,4 +1,25 @@
 import { LightningElement } from "lwc";
 import getAviationData from "@salesforce/apex/AviationService.getAviationData";
 
-export default class AviationService extends LightningElement {}
+export default class AviationService extends LightningElement {
+  limit = 0;
+  isLoading = false;
+
+  handleInputChange(event) {
+    this.limit = event.target.value;
+  }
+
+  getFlightData() {
+    this.isLoading = true;
+    getAviationData({ flightLimit: this.limit })
+      .then((res) => {
+        console.log("Result: ", res);
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
+  }
+}
