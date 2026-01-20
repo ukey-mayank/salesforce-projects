0ff9f501fe248479197a0ebbf605ac080d9092f9
MayankUkey <mayankukey15@gmail.com>
2026-01-20 22:51:34 +0530

    Complete functionality of Smart Contact Creator with email validation

--- force-app/main/default/lwc/smartContactCreator/smartContactCreator.js
+++ force-app/main/default/lwc/smartContactCreator/smartContactCreator.js
@@ -1,3 +1,29 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc";
+import validateEmail from "@salesforce/apex/ContactValidator.validateEmail";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 
-export default class SmartContactCreator extends LightningElement {}
+export default class SmartContactCreator extends LightningElement {
+  handleSubmit(event) {
+    event.preventDefault();
+    const fields = event.detail.fields;
+
+    validateEmail({ email: fields.email })
+      .then(() => {
+        this.template.querySelector("lightning-record-form").submit(fields);
+      })
+      .catch((error) => {
+        this.showToastEvent("Validation Error", error.body.message, "error");
+      });
+  }
+
+  handleSuccess() {
+    this.showToastEvent("Success", "Contact created successfully", "success");
+  }
+  handleError(event) {
+    this.showToastEvent("Error", event.detail.message, "error");
+  }
+
+  showToastEvent(title, message, variant) {
+    this.dispatchEvent(new ShowToastEvent(title, message, variant));
+  }
+}
